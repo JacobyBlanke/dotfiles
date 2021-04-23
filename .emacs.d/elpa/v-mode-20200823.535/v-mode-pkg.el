152b76f35b766224cb978fe28f3a98c50bfc7fb5
Jacoby Blanke <xohvan@gmail.com>
2021-04-22 20:35:07 -0400

    add emacs config

---
+++ .emacs.d/elpa/v-mode-20200823.535/v-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/jacoby/.emacs.d/elpa/v-mode-20200823.535/v-mode.el  -*- no-byte-compile: t -*-
+(define-package "v-mode" "20200823.535" "A major mode for the V programming language" '((emacs "25.1") (dash "2.17.0") (hydra "0.15.0")) :commit "d97fb8de5ab19359029dec1195f3d5b87aeb27b1" :keywords '("languages" "programming") :url "https://github.com/damon-kwok/v-mode")
---
+++ .emacs.d/to-be-dumped.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(kotlin-mode wgrep web-mode vterm vscode-dark-plus-theme v-mode use-package smart-mode-line rainbow-mode pyvenv pdf-tools org-latex-impatient org-bullets neotree modern-cpp-font-lock minions lsp-python-ms lsp-java latex-preview-pane ivy-prescient hl-todo highlight-symbol highlight-numbers highlight-escape-sequences format-all flycheck exec-path-from-shell evil-matchit evil-magit evil-commentary evil-collection emojify emmet-mode elcord dired-subtree dired-single diff-hl cpp-auto-include counsel-projectile company-prescient cobol-mode centaur-tabs auto-complete auctex-latexmk)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
